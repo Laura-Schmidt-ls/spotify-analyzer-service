fc52e3b9b8d8e838f493bb6729ec1b3caba45d5a
Laura-Schmidt-ls <57891286+Laura-Schmidt-ls@users.noreply.github.com>
2020-11-06 21:57:16 +0100

    changed urls

--- src/main/java/com/spotifyanalyzerservice/service/AuthentificationMain.java
+++ src/main/java/com/spotifyanalyzerservice/service/AuthentificationMain.java
@@ -14,14 +14,14 @@ public class AuthentificationMain {
     private static final String CLIENT_ID = "0389937726e547e49bcf1302d5a5e758";
     private static final String CLIENT_SECRET = "083e8e8b0c73498b9705f1f5be826df9";
     private static final String REDIRECT_URL_LOCAL = "https://localhost:8080/redirect";
-    private static final String REDIRECT_URL_HEROKU = "https://spotify-analyzer-service.herokuapp.com/redirect";
+    private static final String REDIRECT_URL_HEROKU = "https://quiet-cove-37008.herokuapp.com/redirect";
 
     public static void main(String[] args) throws IOException {
         SpringApplication.run(AuthentificationMain.class, args);
         callSpotifyService();
     }
 
-    @CrossOrigin(origins = {"https://spotify-analyzer-service.herokuapp.com", "http://localhost:8080"})
+    @CrossOrigin(origins = {"https://quiet-cove-37008.herokuapp.com/", "http://localhost:8080"})
     @GetMapping(path = "/auth")
     public static String callSpotifyService() {
         return SpotifyService.authenticate(CLIENT_ID, REDIRECT_URL_HEROKU);
