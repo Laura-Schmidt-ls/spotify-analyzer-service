d8b4858d237bb6d1af3c79b2f0c3920d0d52731f
Laura-Schmidt-ls <57891286+Laura-Schmidt-ls@users.noreply.github.com>
2020-11-11 18:51:56 +0100

    getting the requests working

---
+++ src/main/java/com/spotifyanalyzerservice/service/AuthenticationMain.java
@@ -0,0 +1,17 @@
+package com.spotifyanalyzerservice.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Starts the Spring Boot Application.
+ *
+ * @author Laura Schmidt
+ */
+@SpringBootApplication
+public class AuthenticationMain {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthenticationMain.class, args);
+    }
+}
