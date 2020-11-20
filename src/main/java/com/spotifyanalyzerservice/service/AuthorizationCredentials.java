01918cd905c058720055b8459b229c8143f1bcfd
Laura-Schmidt-ls <57891286+Laura-Schmidt-ls@users.noreply.github.com>
2020-11-20 14:20:02 +0100

    added file AuthorizationCredentials

---
+++ src/main/java/com/spotifyanalyzerservice/service/AuthorizationCredentials.java
@@ -0,0 +1,43 @@
+package com.spotifyanalyzerservice.service;
+
+public class AuthorizationCredentials {
+    String accessToken;
+    String refreshToken;
+    String expiresIn;
+
+    public AuthorizationCredentials(String accessToken, String refreshToken, String expiresIn) {
+        this.accessToken = accessToken;
+        this.refreshToken = refreshToken;
+        this.expiresIn = expiresIn;
+    }
+
+    public String toJSONString() {
+        return "{\"access-token\": \"" + this.accessToken + "\", "
+                + "\"refresh-token\": \"" + this.refreshToken + "\"}"
+                + "\"expires-in\": \"" + this.expiresIn + "\"}";
+    }
+
+    public String getAccessToken() {
+        return accessToken;
+    }
+
+    public void setAccessToken(String accessToken) {
+        this.accessToken = accessToken;
+    }
+
+    public String getRefreshToken() {
+        return refreshToken;
+    }
+
+    public void setRefreshToken(String refreshToken) {
+        this.refreshToken = refreshToken;
+    }
+
+    public String getExpiresIn() {
+        return expiresIn;
+    }
+
+    public void setExpiresIn(String expiresIn) {
+        this.expiresIn = expiresIn;
+    }
+}
