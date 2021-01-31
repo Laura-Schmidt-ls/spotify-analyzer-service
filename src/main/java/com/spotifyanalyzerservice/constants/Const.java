3347786153098ac09ee8c2356c1c35e7e917b5f7
Laura-Schmidt-ls <lauschmidt.ls@gmail.com>
2021-01-31 10:13:37 +0100

    modified access of CLIENT_ID and CLIENT_SECRET

--- src/main/java/com/spotifyanalyzerservice/constants/Const.java
+++ src/main/java/com/spotifyanalyzerservice/constants/Const.java
@@ -1,8 +1,9 @@
 package com.spotifyanalyzerservice.constants;
 
 public interface Const {
-    String CLIENT_ID = "0389937726e547e49bcf1302d5a5e758";
-    String CLIENT_SECRET = "083e8e8b0c73498b9705f1f5be826df9";
+
+    String CLIENT_ID = System.getenv("SPOTIFY_CLIENT_ID");
+    String CLIENT_SECRET = System.getenv("SPOTIFY_CLIENT_SECRET");
 
     String REDIRECT_URL = "https://spotify-analyzer-service.herokuapp.com/api/user_code/";
 }
